4b69bd57c8ce6cee128200e8a3fa22eb5cf1f3fc
wangcong <wangcong@fsmeeting.com>
2022-05-12 23:52:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div>以 {{ stime }} 作为周期计算开始时间</div>
-  <div class="flex">
-    <input type="text" v-model="syear" :disabled="!editable" maxlength="4" />
-    <div class="split">年</div>
-    <input type="text" v-model="smonth" :disabled="!editable" maxlength="2" />
-    <div class="split">月</div>
-    <input type="text" v-model="sday" :disabled="!editable" maxlength="2" />
-  </div>
-
-  <div class="time-split">到</div>
-
   <div class="flex no-margin">
-    <input type="text" v-model="eyear" :disabled="!editable" maxlength="4" />
+    <div>截至时间：</div>
+    <input type="text" v-model="eyear" maxlength="4" />
     <div class="split">年</div>
-    <input type="text" v-model="emonth" :disabled="!editable" maxlength="2" />
+    <input type="text" v-model="emonth" maxlength="2" />
     <div class="split">月</div>
-    <input type="text" v-model="eday" :disabled="!editable" maxlength="2" />
+    <input type="text" v-model="eday" maxlength="2" />
   </div>
 
-  <button class="btn" @click="editable = !editable">{{ !editable ? '设置日期' : '取消' }}</button>
-
+  <div class="title">临床孕周，以 {{ lstime }} 作为周期计算开始时间</div>
   <div class="flex">
-    <div>当前周期：{{ cycle.weeks }}w</div>
-    <div v-if="cycle.days > 0">&nbsp;+ {{ cycle.days }}</div>
-    <div>（ {{ cycle.allWeeks }}w ），</div>
-    <div>已 {{ cycle.allDays }} 天。</div>
+    <input type="text" v-model="lsyear" maxlength="4" />
+    <div class="split">年</div>
+    <input type="text" v-model="lsmonth" maxlength="2" />
+    <div class="split">月</div>
+    <input type="text" v-model="lsday" maxlength="2" />
+  </div>
+
+  <div class="flex text-red">
+    <div>临床孕周：{{ lcycle.weeks }}w</div>
+    <div v-if="lcycle.days > 0">&nbsp;+ {{ lcycle.days }}</div>
+    <div>（ {{ lcycle.allWeeks }}w ），</div>
+    <div>已 {{ lcycle.allDays }} 天。</div>
+  </div>
+
+  <div class="title">超声孕周，以 {{ cstime }} 作为周期计算开始时间</div>
+  <div class="flex">
+    <input type="text" v-model="csyear" maxlength="4" />
+    <div class="split">年</div>
+    <input type="text" v-model="csmonth" maxlength="2" />
+    <div class="split">月</div>
+    <input type="text" v-model="csday" maxlength="2" />
+  </div>
+
+  <div class="flex text-red">
+    <div>超声孕周：{{ ccycle.weeks }}w</div>
+    <div v-if="ccycle.days > 0">&nbsp;+ {{ ccycle.days }}</div>
+    <div>（ {{ ccycle.allWeeks }}w ），</div>
+    <div>已 {{ ccycle.allDays }} 天。</div>
   </div>
 </template>
 
@@ -34,19 +47,37 @@ import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 dayjs.extend(duration);
 
-const editable = ref(false);
-const syear = ref(2021);
-const smonth = ref(11);
-const sday = ref(30);
 const eyear = ref(dayjs().year());
 const emonth = ref(dayjs().month() + 1);
 const eday = ref(dayjs().date());
 
-const stime = computed(() => `${syear.value}-${smonth.value}-${sday.value} 00:00:00`);
-const etime = computed(() => `${eyear.value}-${emonth.value}-${eday.value} 00:00:00`);
+const lsyear = ref(2021);
+const lsmonth = ref(11);
+const lsday = ref(25);
 
-const cycle = computed(() => {
-  const timeDuration = dayjs.duration(dayjs(etime.value).valueOf() - dayjs(stime.value).valueOf());
+const csyear = ref(2021);
+const csmonth = ref(11);
+const csday = ref(30);
+
+const lstime = computed(() => `${lsyear.value}-${lsmonth.value}-${lsday.value}`);
+const cstime = computed(() => `${csyear.value}-${csmonth.value}-${csday.value}`);
+const etime = computed(() => `${eyear.value}-${emonth.value}-${eday.value}`);
+
+const lcycle = computed(() => {
+  const timeDuration = dayjs.duration(dayjs(etime.value).valueOf() - dayjs(lstime.value).valueOf());
+  const allDays = Math.ceil(timeDuration.asDays());
+  const allWeeks = timeDuration.asWeeks().toFixed(1);
+  let weeks = Math.floor(allWeeks);
+  let days = allDays - weeks * 7;
+  if (days >= 7) {
+    days = 0;
+    weeks += 1;
+  }
+  return { allDays, allWeeks, weeks, days };
+});
+
+const ccycle = computed(() => {
+  const timeDuration = dayjs.duration(dayjs(etime.value).valueOf() - dayjs(cstime.value).valueOf());
   const allDays = Math.ceil(timeDuration.asDays());
   const allWeeks = timeDuration.asWeeks().toFixed(1);
   let weeks = Math.floor(allWeeks);
@@ -66,12 +97,12 @@ const cycle = computed(() => {
   box-sizing: border-box;
 }
 #app {
-  padding: 30px;
+  padding: 20px;
 }
 .flex {
   display: flex;
   align-items: center;
-  margin-top: 30px;
+  margin-top: 15px;
 }
 input {
   width: 60px;
@@ -91,16 +122,10 @@ input {
 .time-split {
   margin: 10px 0;
 }
-.btn {
-  width: 80px;
-  border-radius: 4px;
-  outline: none;
-  border: none;
-  background: #1890ff;
-  color: #fff;
-  padding: 0 5px;
-  cursor: pointer;
-  margin-top: 30px;
-  line-height: 28px;
+.title {
+  margin-top: 50px;
+}
+.text-red {
+  color: #f00;
 }
 </style>
